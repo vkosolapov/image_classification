00effde2335edaf1879c66a22d67a48f5e1ac2e3
Vladimir Kosolapov <vladimir_kosolapov@epam.com>
2021-12-05 15:35:43 +0300

    Metrics and Logging

--- image_classification/train.py
+++ image_classification/train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+from torchmetrics import AUROC
 from torch.utils.tensorboard import SummaryWriter
 
 import numpy as np
@@ -28,38 +29,60 @@ num_classes = 10
 batch_size = 64
 num_epochs = 20
 pretrained = False
+auroc = AUROC(num_classes=num_classes, average="macro")
 
 
-def evaluate_minibatch(preds, labels, loss, running_loss, running_corrects, inputs_size):
+def evaluate_minibatch(preds, probs, labels, loss, running_loss, running_corrects, running_auc, inputs_size):
+    acc = torch.sum(preds == labels.data) / len(preds)
+    auc = auroc(probs, labels)
     running_loss += loss.item() * inputs_size
     running_corrects += torch.sum(preds == labels.data)
-    acc = torch.sum(preds == labels.data) / len(preds)
-    return running_loss, running_corrects, acc
+    running_auc += auc
+    return acc, auc, running_loss, running_corrects, running_auc
 
 
-def log_minibatch(phase, loss, acc, dataset_size, epoch, minibatch):
+def log_minibatch(phase, loss, acc, auc, dataset_size, epoch, minibatch):
     writer.add_scalar(f"batch_loss/{phase}", loss.item(), dataset_size // batch_size * epoch + minibatch)
     writer.add_scalar(f"batch_acc/{phase}", acc, dataset_size // batch_size * epoch + minibatch)
+    writer.add_scalar(f"batch_auc/{phase}", auc, dataset_size // batch_size * epoch + minibatch)
     writer.close()
 
 
-def evaluate_epoch(running_loss, running_corrects, dataset_size):
+def evaluate_epoch(running_loss, running_corrects, running_auc, dataset_size):
     epoch_loss = running_loss / dataset_size
     epoch_acc = running_corrects.double() / dataset_size
-    return epoch_loss, epoch_acc
+    epoch_auc = running_auc / dataset_size * batch_size
+    return epoch_loss, epoch_acc, epoch_auc
 
 
-def log_epoch(phase, epoch_loss, epoch_acc, epoch):
+def log_epoch(phase, epoch_loss, epoch_acc, epoch_auc, epoch):
     writer.add_scalar(f"epoch_loss/{phase}", epoch_loss, epoch)
     writer.add_scalar(f"epoch_acc/{phase}", epoch_acc, epoch)
+    writer.add_scalar(f"epoch_auc/{phase}", epoch_auc, epoch)
     writer.close()
     print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+
+def log_norm(model, dataset_size, epoch, minibatch):
+    total_param_norm = 0
+    total_grad_norm = 0
+    for p in model.parameters():
+        param_norm = p.detach().data.norm(2)
+        total_param_norm += param_norm.item() ** 2
+        grad_norm = p.grad.detach().data.norm(2)
+        total_grad_norm += grad_norm.item() ** 2
+    total_param_norm = total_param_norm ** (0.5)
+    total_grad_norm = total_grad_norm ** (0.5)
+    writer.add_scalar(f"norm/param", total_param_norm, dataset_size // batch_size * epoch + minibatch)
+    writer.add_scalar(f"norm/grad", total_grad_norm, dataset_size // batch_size * epoch + minibatch)
+    writer.close()
 
 
 def train_epoch(model, data_loader, criterion, optimizer, scheduler, epoch):
     model.train()
     running_loss = 0.0
     running_corrects = 0
+    running_auc = 0.0
     for i, (inputs, labels) in tqdm(enumerate(data_loader.data_loader)):
         inputs = inputs.to(device)
         labels = labels.to(device)
@@ -67,21 +90,25 @@ def train_epoch(model, data_loader, criterion, optimizer, scheduler, epoch):
             outputs = model(inputs)
             _, preds = torch.max(outputs, 1)
             loss = criterion(outputs, labels)
+            probs = torch.softmax(outputs.detach(), 1)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        running_loss, running_corrects, acc = evaluate_minibatch(
+        acc, auc, running_loss, running_corrects, running_auc = evaluate_minibatch(
             preds, 
+            probs,
             labels, 
             loss, 
             running_loss, 
             running_corrects, 
+            running_auc,
             inputs.size(0), 
         )
-        log_minibatch("train", loss, acc, data_loader.dataset_size, epoch, i)
+        log_minibatch("train", loss, acc, auc, data_loader.dataset_size, epoch, i)
+        log_norm(model, data_loader.dataset_size, epoch, i)
     scheduler.step()
-    epoch_loss, epoch_acc = evaluate_epoch(running_loss, running_corrects, data_loader.dataset_size)
-    log_epoch("train", epoch_loss, epoch_acc, epoch)
+    epoch_loss, epoch_acc, epoch_auc = evaluate_epoch(running_loss, running_corrects, running_auc, data_loader.dataset_size)
+    log_epoch("train", epoch_loss, epoch_acc, epoch_auc, epoch)
     checkpoint = {
         "epoch": epoch,
         "model_state": model.state_dict(),
@@ -95,6 +122,7 @@ def test_epoch(model, data_loader, criterion, epoch):
     model.eval()
     running_loss = 0.0
     running_corrects = 0
+    running_auc = 0.0
     for i, (inputs, labels) in tqdm(enumerate(data_loader.data_loader)):
         inputs = inputs.to(device)
         labels = labels.to(device)
@@ -102,17 +130,20 @@ def test_epoch(model, data_loader, criterion, epoch):
             outputs = model(inputs)
             _, preds = torch.max(outputs, 1)
             loss = criterion(outputs, labels)
-        running_loss, running_corrects, acc = evaluate_minibatch(
+            probs = torch.softmax(outputs.detach(), 1)
+        acc, auc, running_loss, running_corrects, running_auc = evaluate_minibatch(
             preds, 
+            probs,
             labels, 
             loss, 
             running_loss, 
             running_corrects, 
+            running_auc,
             inputs.size(0), 
         )
-        log_minibatch("val", loss, acc, data_loader.dataset_size, epoch, i)
-    epoch_loss, epoch_acc = evaluate_epoch(running_loss, running_corrects, data_loader.dataset_size)
-    log_epoch("val", epoch_loss, epoch_acc, epoch)
+        log_minibatch("val", loss, acc, auc, data_loader.dataset_size, epoch, i)
+    epoch_loss, epoch_acc, epoch_auc = evaluate_epoch(running_loss, running_corrects, running_auc, data_loader.dataset_size)
+    log_epoch("val", epoch_loss, epoch_acc, epoch_auc, epoch)
     return epoch_acc
 
 
