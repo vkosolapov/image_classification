97a137c6294d6cc9d8551374a032958e33fe8dd3
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-11 23:41:40 +0300

    EfficientNet

--- image_classification/train.py
+++ image_classification/train.py
@@ -43,7 +43,8 @@ if __name__ == "__main__":
         avg_down=True,
         num_classes=num_classes,
     )
-    model = _create_resnet("seresnext50_32x4d", False, **model_args).to(device)
+    # model = _create_resnet("seresnext50_32x4d", False, **model_args).to(device)
+    model = create_model("efficientnet_b0", num_classes=num_classes).to(device)
 
     optimizer = Ranger(model.parameters(), lr=0.01, weight_decay=0.0001)
     # swa = SWA(optimizer_conv, swa_start=10, swa_freq=5, swa_lr=0.05)
@@ -124,7 +125,7 @@ if __name__ == "__main__":
     )
 
     loop = TrainLoop(
-        experiment_name="016_SE_SK_Res2NeXt_D",
+        experiment_name="017_efficientnet_b0",
         device=device,
         datadir="data/imagenette2",
         batch_size=64,
