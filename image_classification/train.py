4ecedf449f626b2139794be220ad11975aba2ecf
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-05-04 10:00:30 +0300

    RepVGG

--- image_classification/train.py
+++ image_classification/train.py
@@ -26,7 +26,7 @@ np.random.seed(0)
 torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
 
-EXPERIMENT_NAME = "008_nf_ecaresnet50"
+EXPERIMENT_NAME = "009_repvgg_a2"
 wandb.init(sync_tensorboard=True, project="image_classification", name=EXPERIMENT_NAME)
 
 if __name__ == "__main__":
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         num_classes=num_classes,
     )
     # model = _create_resnet("ecaresnet50d", False, **model_args).to(device)
-    model = create_model("nf_ecaresnet50", num_classes=num_classes).to(device)
+    model = create_model("repvgg_a2", num_classes=num_classes).to(device)
 
     optimizer = Ranger(model.parameters(), lr=0.01, weight_decay=0.0001)
     # swa = SWA(optimizer_conv, swa_start=10, swa_freq=5, swa_lr=0.05)
